646d9563c4488e5ec16a19ec725d05a9e62a8648
JakusDani <skelox0@gmail.com>
2023-03-28 00:32:28 +0200

    test netlify with next

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <h1>Hello world</h1>
+        <h1>Hello world!!4</h1>
       </main>
     </>
   );
